2cad8ecdfad7b88f5dcbe84a9ed02744a14d1a81
Jaylan <jaylanwienckowski@gmail.com>
2020-07-07 11:23:34 -0500

    Structured switch statements and functions

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const fs = require("fs");
 
 // Connecting to MySQL
 const connection = mysql.createConnection({
@@ -16,3 +15,92 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+
+function manageTeam() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View Employees",
+        "View Employees by Department",
+        "View Employees by Manager",
+        "Add Emolyee",
+        "Remove Emplyee",
+        "Update Employee Role",
+        "Update Manager Role",
+        "exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "View Employees":
+        viewEmployee();
+        break;
+
+      case "View Employees by Department":
+        viewEmployeeDep();
+        break;
+
+      case "View Employees by Manager":
+        viewEmployeeMan();
+        break;
+
+      case "Add Employee":
+        addEmployee();
+        break;
+
+      case "Remove Employee":
+        removeEmployee();
+        break;
+      
+      case "Update Employee Role":
+        updateEmployee();
+        break;
+
+      case "Update Manager Role":
+        updateManager();
+        break;
+
+      case "exit":
+        connection.end();
+        break;
+      }
+    });
+}
+
+function viewEmployee() {
+
+        manageTeam();
+}
+
+function viewEmployeeDep() {
+
+        manageTeam();
+}
+
+function viewEmployeeMan() {
+
+        manageTeam();
+}
+
+function addEmployee() {
+
+        manageTeam();
+}
+
+function removeEmployee () {
+
+        manageTeam();
+}
+
+function updateEmployee() {
+
+        manageTeam();
+}
+
+function updateManager() {
+
+        manageTeam();
+}
